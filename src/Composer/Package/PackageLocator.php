0b7e80aa7906237aa7d3debee6e18c3c9cf43a86
Josh Parkinson <joshparkinson1991@gmail.com>
2020-06-01 17:23:07 +0100

    PackageLocator Service

---
+++ src/Composer/Package/PackageLocator.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * PackageLocator.php
+ */
+
+namespace JParkinson1991\ComposerLinkerPlugin\Composer\Package;
+
+use Composer\Package\PackageInterface;
+use Composer\Repository\RepositoryInterface;
+use InvalidArgumentException;
+
+/**
+ * Class PackageLocator
+ *
+ * @package JParkinson1991\ComposerLinkerPlugin\Composer\Package
+ */
+class PackageLocator implements PackageLocatorInterface
+{
+    /**
+     * Returns a package from a given repository by name
+     *
+     * @param string $packageName
+     *     The name of the package
+     * @param \Composer\Repository\RepositoryInterface $repository
+     *     The repository to get the package from
+     *
+     * @return \Composer\Package\PackageInterface
+     *     The found package
+     *
+     * @throws \InvalidArgumentException
+     *     If the package not found
+     */
+    public function getFromRepository(string $packageName, RepositoryInterface $repository): PackageInterface
+    {
+        $foundPackages = $repository->findPackages($packageName);
+
+        if (count($foundPackages) === 0) {
+            throw new InvalidArgumentException(
+                'Failed to find package <info>'.$packageName.'</info>'
+            );
+        }
+
+        if (count($foundPackages) > 1) {
+            throw new InvalidArgumentException(
+                'Found multiple packages for <info>'.$packageName.'</info>. Be more specific'
+            );
+        }
+
+        return $foundPackages[0];
+    }
+
+}
