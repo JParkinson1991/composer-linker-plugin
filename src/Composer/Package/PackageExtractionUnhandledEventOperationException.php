ed3c3469a19fe005c1d131bd85d2881d3b33d219
Josh Parkinson <joshparkinson1991@gmail.com>
2020-06-02 18:11:35 +0100

    Use import

--- src/Composer/Package/PackageExtractionUnhandledEventOperationException.php
+++ src/Composer/Package/PackageExtractionUnhandledEventOperationException.php
@@ -8,12 +8,14 @@ declare(strict_types=1);
 
 namespace JParkinson1991\ComposerLinkerPlugin\Composer\Package;
 
+use Exception;
+
 /**
  * Class PackageExtractionUnhandledEventOperationException
  *
  * @package JParkinson1991\ComposerLinkerPlugin\Composer\Package
  */
-class PackageExtractionUnhandledEventOperationException extends \Exception
+class PackageExtractionUnhandledEventOperationException extends Exception
 {
 
 }
