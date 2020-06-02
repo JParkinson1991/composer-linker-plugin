fe600d33999c2f0c768e4c9b2a3124887823a82a
Josh Parkinson <joshparkinson1991@gmail.com>
2020-06-02 15:08:13 +0100

    Link command integration test

---
+++ tests/Integration/Composer/Commands/BaseCommandTest.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @file
+ * BaseCommandTest.php
+ */
+
+namespace JParkinson1991\ComposerLinkerPlugin\Tests\Integration\Composer\Commands;
+
+use Composer\Console\Application;
+use Composer\EventDispatcher\EventDispatcher;
+use JParkinson1991\ComposerLinkerPlugin\Link\LinkDefinitionFactory;
+use JParkinson1991\ComposerLinkerPlugin\Tests\Integration\Composer\BaseComposerTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class BaseCommandTest
+ *
+ * Provides a base for command test and shareable test cases that should be
+ * command execution agnostic
+ *
+ * @package JParkinson1991\ComposerLinkerPlugin\Tests\Integration\Composer\Commands
+ */
+abstract class BaseCommandTest extends BaseComposerTestCase
+{
+    /**
+     * @var CommandTester
+     */
+    protected $commandTester;
+
+    /**
+     * Returns the command instance to text
+     *
+     * @return mixed
+     */
+    abstract protected function getCommandInstance();
+
+    /**
+     * Sets up the command
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $composer = $this->getComposer();
+        $composer
+            ->method('getEventDispatcher')
+            ->willReturn($this->createMock(EventDispatcher::class));
+
+        $application = $this->getMockBuilder(Application::class)
+            ->onlyMethods(['getComposer'])
+            ->getMock();
+
+        $application
+            ->method('getComposer')
+            ->willReturn($composer);
+
+        $command = $this->getCommandInstance();
+        $application->add($command);
+        $command = $application->find($command->getName());
+
+        $this->commandTester = new CommandTester($command);
+    }
+
+    /**
+     * Tests that it errors when package not installed.
+     *
+     * Dont initialise any packages, just run the command, composer will act
+     * as if nothing is installed and this command should error
+     *
+     * The follow test should be command execution agnostic
+     *
+     * @return void
+     */
+    public function testItErrorsOnPackageNotInstalled(): void
+    {
+        $exitCode = $this->commandTester->execute([
+            'package-names' => [
+                'unknown/package'
+            ]
+        ]);
+
+        // Assert command error
+        $this->assertSame(1, $exitCode);
+        $this->assertStringContainsStringIgnoringCase(
+            'failed to find package unknown/package',
+            $this->commandTester->getDisplay()
+        );
+    }
+
+    /**
+     * Tests that it errors if a package is installed but not configured
+     *
+     * If someone executes a command for a package that has no config it
+     * should throw an error
+     *
+     * The follow test should be command execution agnostic
+     *
+     * @return void
+     */
+    public function testItErrorsIfConfigNotFound(): void
+    {
+        $this->initialisePackage(
+            'test/package',
+            'test-package',
+            []
+        );
+
+        $exitCode = $this->commandTester->execute([
+            'package-names' => [
+                'test/package'
+            ]
+        ]);
+
+        // Assert command error
+        $this->assertSame(1, $exitCode);
+        $this->assertStringContainsStringIgnoringCase(
+            'no config found for package test/package',
+            $this->commandTester->getDisplay()
+        );
+    }
+
+    /**
+     * Tests that the command throws error if run on a package with invalid
+     * configuration
+     *
+     * The follow test should be command execution agnostic
+     *
+     * @return void
+     */
+    public function testItErrorsOnInvalidConfig(): void
+    {
+        $this->initialisePackage(
+            'test/package',
+            'test-package',
+            []
+        );
+
+        $this->setPluginConfig([
+            LinkDefinitionFactory::CONFIG_KEY_ROOT => [
+                LinkDefinitionFactory::CONFIG_KEY_LINKS => [
+                    'test/package' => [
+                        'no-dir-key' => 'here'
+                    ]
+                ]
+            ]
+        ]);
+
+        $exitCode = $this->commandTester->execute([
+            'package-names' => [
+                'test/package'
+            ]
+        ]);
+
+        // Assert command error
+        $this->assertSame(1, $exitCode);
+    }
+}
---
+++ tests/Integration/Composer/Commands/LinkCommandTest.php
@@ -0,0 +1,263 @@
+<?php
+/**
+ * @file
+ * LinkCommandTest.php
+ */
+
+namespace JParkinson1991\ComposerLinkerPlugin\Tests\Integration\Composer\Commands;
+
+use JParkinson1991\ComposerLinkerPlugin\Composer\Commands\LinkCommand;
+use JParkinson1991\ComposerLinkerPlugin\Composer\Package\PackageLocator;
+use JParkinson1991\ComposerLinkerPlugin\Link\LinkDefinitionFactory;
+
+/**
+ * Class LinkCommandTest
+ *
+ * @package JParkinson1991\ComposerLinkerPlugin\Tests\Integration\Composer\Commands
+ */
+class LinkCommandTest extends BaseCommandTest
+{
+    /**
+     * Returns the command instance to text
+     *
+     * @return mixed
+     */
+    protected function getCommandInstance()
+    {
+        return new LinkCommand(new PackageLocator());
+    }
+
+    /**
+     * Tests that the command links all viable packages within a repository
+     * when run without arguments
+     *
+     * @return void
+     */
+    public function testItLinksARepositoryWhenRunWithoutArguments(): void
+    {
+        $this->initialisePackage(
+            'package/one',
+            'package-one',
+            [
+                'README.md',
+                'src/Class1.php'
+            ]
+        );
+
+        $this->initialisePackage(
+            'package/two',
+            'package-two',
+            [
+                'README.md',
+                'src/Class2.php'
+            ]
+        );
+
+        $this->initialisePackage(
+            'package/three',
+            'package-three',
+            [
+                'README.md',
+                'src/Class3.php'
+            ]
+        );
+
+        $this->setPluginConfig([
+            LinkDefinitionFactory::CONFIG_KEY_ROOT => [
+                LinkDefinitionFactory::CONFIG_KEY_LINKS => [
+                    'package/one' => 'linked-package-one',
+                    'package/two' => [
+                        LinkDefinitionFactory::CONFIG_KEY_LINKS_DIR => 'linked-package-two',
+                        LinkDefinitionFactory::CONFIG_KEY_LINKS_FILES => [
+                            'src/Class2.php'
+                        ],
+                        LinkDefinitionFactory::CONFIG_KEY_OPTIONS => [
+                            LinkDefinitionFactory::CONFIG_KEY_OPTIONS_COPY => true
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+
+        $exitCode = $this->commandTester->execute([]);
+
+        // Assert command successful
+        $this->assertSame(0, $exitCode);
+
+        // Assert package one link existing
+        $this->assertFileExists($this->getAbsolutePath('linked-package-one'));
+        $this->assertTrue(is_link($this->getAbsolutePath('linked-package-one')));
+        $this->assertFileExists($this->getAbsolutePath('linked-package-one/README.md'));
+        $this->assertFileExists($this->getAbsolutePath('linked-package-one/src/Class1.php'));
+
+        // Asset package two link existing
+        $this->assertFileExists($this->getAbsolutePath('linked-package-two'));
+        $this->assertFileExists($this->getAbsolutePath('linked-package-two/src/Class2.php'));
+        $this->assertFalse(is_link($this->getAbsolutePath('linked-package-two/src/Class2.php')));
+        $this->assertFileDoesNotExist($this->getAbsolutePath('linked-package-two/README.md'));
+
+        // Assert package three not linked
+        $this->assertFileDoesNotExist($this->getAbsolutePath('linked-package-three'));
+    }
+
+    /**
+     * Tests that when the command is run without arguments thus linking the
+     * entire repository that errors do not break execution, but are still
+     * handled and sent to the caller
+     *
+     * @return void
+     */
+    public function testRepositoryLinkErrorsDontBreakProcessButOutputAfter(): void
+    {
+        $this->initialisePackage(
+            'package/one',
+            'package-one',
+            []
+        );
+
+        $this->initialisePackage(
+            'package/two',
+            'package-two',
+            []
+        );
+
+        $this->initialisePackage(
+            'package/three',
+            'package-three',
+            []
+        );
+
+        $this->setPluginConfig([
+            LinkDefinitionFactory::CONFIG_KEY_ROOT => [
+                LinkDefinitionFactory::CONFIG_KEY_LINKS => [
+                    'package/one' => 'linked-package-one',
+                    'package/two' => [
+                        'invalid-config' => 'here'
+                    ],
+                    'package/three' => 'linked-package-three'
+                ]
+            ]
+        ]);
+
+        $exitCode = $this->commandTester->execute([]);
+
+        // Assert command error
+        $this->assertSame(1, $exitCode);
+        $this->assertStringContainsStringIgnoringCase(
+            'process completed with errors',
+            $this->commandTester->getDisplay()
+        );
+
+        // Assert valid packages linked
+        $this->assertFileExists($this->getAbsolutePath('linked-package-one'));
+        $this->assertFileExists($this->getAbsolutePath('linked-package-three'));
+    }
+
+    /**
+     * Tests that packages passed as arguments by name
+     *
+     * @return void
+     */
+    public function testItLinksPackagesPassedAsArguments(): void
+    {
+        $this->initialisePackage(
+            'package/one',
+            'package-one',
+            []
+        );
+
+        $this->initialisePackage(
+            'package/two',
+            'package-two',
+            []
+        );
+
+        $this->initialisePackage(
+            'package/three',
+            'package-three',
+            []
+        );
+
+        $this->setPluginConfig([
+            LinkDefinitionFactory::CONFIG_KEY_ROOT => [
+                LinkDefinitionFactory::CONFIG_KEY_LINKS => [
+                    'package/one' => 'linked-package-one',
+                    'package/two' => 'linked-package-two',
+                    'package/three' => 'linked-package-three'
+                ]
+            ]
+        ]);
+
+        $exitCode = $this->commandTester->execute([
+            'package-names' => [
+                'package/one',
+                'package/three'
+            ]
+        ]);
+
+        // Assert command successful
+        $this->assertSame(0, $exitCode);
+
+        // Assert package one files exist
+        $this->assertFileExists($this->getAbsolutePath('linked-package-one'));
+
+        // Assert package two files dont exist despite plugin config existing
+        // Command when given arguments should only link those files
+        $this->assertFileDoesNotExist($this->getAbsolutePath('linked-package-two'));
+
+        // Assert package three files exist
+        $this->assertFileExists($this->getAbsolutePath('linked-package-three'));
+    }
+
+    /**
+     * Test that package link error do not break execution.
+     *
+     * If multiple arguments are passed and some trigger errors still attempt
+     * to process the valid arguments.
+     *
+     * @void
+     */
+    public function testPackageLinkErrorsDontBreakExecution(): void
+    {
+        $this->initialisePackage(
+            'package/one',
+            'package-one',
+            []
+        );
+
+        $this->initialisePackage(
+            'package/two',
+            'package-two',
+            []
+        );
+
+        $this->setPluginConfig([
+            LinkDefinitionFactory::CONFIG_KEY_ROOT => [
+                LinkDefinitionFactory::CONFIG_KEY_LINKS => [
+                    'package/one' => 'linked-package-one',
+                    'package/two' => [
+                        'invalid-config'
+                    ]
+                ]
+            ]
+        ]);
+
+        $exitCode = $this->commandTester->execute([
+            'package-names' => [
+                'uninstalled/package',
+                'package/one',
+                'package/three'
+            ]
+        ]);
+
+        // Assert command error
+        $this->assertSame(1, $exitCode);
+        $this->assertStringContainsStringIgnoringCase(
+            'process completed with errors',
+            $this->commandTester->getDisplay()
+        );
+
+        // Assert valid package linked
+        $this->assertFileExists($this->getAbsolutePath('linked-package-one'));
+    }
+}
